e3135962786acdeb76ec7ee25235be38fe113c38
Pavel <boxiphoto@yandex.ru>
2020-10-24 22:07:41 +0300

    lesson 1 first

--- HomeWork/lesson1/TESTs.py
+++ HomeWork/lesson1/TESTs.py
@@ -1,3 +1,4 @@
+"""
 first_result = int(input('Введите результат 1 дня\n>>>'))
 wish_result = int(input('Введите желаемый результат\n>>>'))
 exp = 0.1
@@ -7,4 +8,21 @@ while day_count < wish_result:
      continue
 if day_count >= wish_result:
     print('Вы достигли результата!')
+"""
+"""
+x = int(input('Введите результат 1 дня\n>>>'))
+y = int(input('Введите желаемый результат\n>>>'))
+day = 1
+while y - x > 0:
+    x = x + (x * 0.1)
+    day += 1
+print(day)
+"""
 
+x = int(input('Введите результат 1 дня\n>>>'))
+y = int(input('Введите желаемый результат\n>>>'))
+i = 1
+while x < y:
+    x *= 1.1
+    i += 1
+print(i)
